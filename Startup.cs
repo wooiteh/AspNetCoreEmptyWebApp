5f26be98cdbcf8bc26d8a2a486fd4a1bb0351cc3
wooiteh <33345615+wooiteh@users.noreply.github.com>
2021-12-07 12:57:14 +0800

    master

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace AspNetCoreEmptyWebApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World !");
+                    await context.Response.WriteAsync("Hello World william !");
                 });
             });
         }
